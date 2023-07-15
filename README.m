f8516709b325f81d49f9156b48b84f3a5cf01ffe
Getlitgitqik <139600067+Getlitgitqik@users.noreply.github.com>
2023-07-14 22:21:32 -0400

    Create README.m
    Idk

---
+++ README.m
@@ -0,0 +1,6 @@
+- 👋 Hi, I’m @Getlitgitqik
+- 👀 I’m interested in learning and want to absorb as much as i can.
+- 🌱 I’m currently learning everything, i know abosolutely nothing.
+
+- 📫 How to reach me is unavailable
+
